68c6880a26d02f55d883a2435f9717e65c189664
anneloremaes <annelore.maes1989@gmail.com>
2020-08-07 16:47:43 +0200

    step 4: Directory and file organization

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,11 +1,5 @@
-angular.module('phonecatApp').component('phoneList', {
-    template: '<ul>' +
-        '<li ng-repeat="phone in $ctrl.phones">' +
-        '<span>{{phone.name}}</span>' +
-        '<p>{{phone.snippet}}</p>' +
-        '</li>' +
-        '</ul>' +
-        '<p>Total number of phones: {{$ctrl.phones.length}}</p>',
+angular.module('phoneList').component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController() {
         this.phones = [{
                 name: 'Nexus S',
--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,5 +1,5 @@
 describe('phoneList', function() {
-    beforeEach(module('phonecatApp'));
+    beforeEach(module('phoneList'));
     describe('phoneListController', function() {
         it('should create `phones` model with 3 phones', inject(function($componentController) {
             var ctrl = $componentController('phoneList');
