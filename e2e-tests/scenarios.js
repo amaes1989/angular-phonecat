0b4aaacaf706084fbd19bfe487d281ab57f3d0a9
anneloremaes <annelore.maes1989@gmail.com>
2020-08-10 13:52:51 +0200

    step 5: filtering repeaters

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,12 +1,34 @@
 'use strict';
 
+const { browser } = require("protractor");
+
 // AngularJS E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
 describe('My app', function() {
 
-  beforeEach(function() {
-    browser.get('index.html');
-  });
+    beforeEach(function() {
+        browser.get('index.html');
+    });
 
+});
+
+describe('PhoneCat Application', function() {
+    describe('phoneList', function() {
+        beforeEach(function() {
+            browser.get('index.html');
+        });
+        it('should filter the phone list as a user types into the search box', function() {
+            var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+            var query = elementt(by.model('$ctrl.query'));
+
+            expect(phoneList.count()).toBe(3);
+
+            query.sendKeys('nexus');
+            expect(phoneList.count()).toBe(1);
+
+            query.sendKeys('motorola');
+            expect(phoneList.count()).toBe(3);
+        });
+    });
 });
